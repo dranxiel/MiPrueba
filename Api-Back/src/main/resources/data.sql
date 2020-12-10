a55b90f20a412cda3d6cd7fe4858739de293bd97
dranxiel <61016770+dranxiel@users.noreply.github.com>
2020-12-09 21:37:50 -0300

    limpieza del plugin
    limpieza del plugin

--- Api-Back/src/main/resources/data.sql
+++ Api-Back/src/main/resources/data.sql
@@ -3,5 +3,6 @@ values (200, 'usuario', 'registrado', CURRENT_TIMESTAMP, true);
 
 insert into Tarea (ID, nombre, descripcion, fecha, activo)
 values (300, 'usuario 2', 'todo los usuarios', CURRENT_TIMESTAMP, false);
-
+insert into Tarea (ID, nombre, descripcion, fecha, activo)
+values (400, 'usuario 3', 'comer', CURRENT_TIMESTAMP, false);
 	
